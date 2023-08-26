4864723a8a12edb020525056f79ba9ae2f7e6cc7
Ramprit Sahani <rpsahani@outlook.in>
2023-08-26 17:32:11 +0530

    first commit

---
+++ app/(main)/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+export default function Home() {
+  return (
+    <main className="flex justify-between mx-10 py-10 ">
+      <p>Get started by editing&nbsp;</p>
+      <ModeToggle />
+    </main>
+  );
+}
