0ca7008e306f762bb1f306838ca6eabb109bcba7
Ramprit Sahani <rpsahani@outlook.in>
2023-08-30 08:49:32 +0530

    Adding db

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const db = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
 export default db;
